d7108d1dbcb62f1b8c3f39852189823b3dd22b31
EbtehalNeseem <93842726+EbtehalNeseem@users.noreply.github.com>
2023-02-06 21:43:12 +0200

    Create Arabic_speech.py

---
+++ Arabic_speech.py
@@ -0,0 +1,17 @@
+from googletrans import Translator
+from gtts import gTTS
+import playsound
+
+
+# Initialize the Translator object
+translator = Translator(service_urls=['translate.google.com'])
+
+# Translate the text
+input_text = "Hello, how are you?"
+translated_text = translator.translate(input_text, dest='ar').text
+
+# convert into speech
+speech = gTTS(text=translated_text, lang='ar', slow=False)
+speech.save("hello.mp3")
+
+playsound.playsound("hello.mp3")
